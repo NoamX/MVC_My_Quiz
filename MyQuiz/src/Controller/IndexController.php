a309b87b344174e2ce3e940bf213f2b283ce1184
noam.dst <dos-santos.noam@epitech.eu>
2020-04-28 20:32:52 +0200

    on est la

--- MyQuiz/src/Controller/IndexController.php
+++ MyQuiz/src/Controller/IndexController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Categorie;
+use Symfony\Component\Security\Core\Security;
+use App\Repository\CategorieRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\ReponseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,14 +14,37 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(CategorieRepository $repository)
     {
-        $categorie = $this->getDoctrine()
-            ->getRepository(Categorie::class)
-            ->findAll();
+        $categorie = $repository->findAll();
+
         return $this->render('base.html.twig', [
             'user' => $this->getUser(),
             'categorie' => $categorie,
+        ]);
+    }
+
+    /**
+     * @Route("/question/{id}", name="question")
+     */
+    public function showQuestion(QuestionRepository $questionRepository, ReponseRepository $reponseRepository, $id)
+    {
+        $question = $questionRepository->findBy(['id_categorie' => $id]);
+
+        // echo '<pre>';
+        // var_dump($reponse);
+        // echo '</pre>';
+
+        // foreach ($question as $value1) {
+        //     echo $value1->getQuestion() . '<br>';
+        // }
+        foreach ($reponse as $value2) {
+            echo $value2->getReponse() . ' ';
+        }
+
+        return $this->render('quiz/question.html.twig', [
+            'question' => $question,
+            // 'reponse' => $reponse,
         ]);
     }
 }
